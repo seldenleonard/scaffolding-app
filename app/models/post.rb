fea126971078617c59d3458c8c809de16f58374b
seldenleonard <selden222@gmail.com>
2020-10-05 00:25:48 -0700

    initial commit

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+
+  # attr_accessible :content, :name, :title
+
+  validates :name, :presence => true
+  validates :title, :presence => true,
+                    :length => { :minimum => 5 }
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Post.create(name: "John", title: "Hello", content: "Hi, I'm new here. Nice to join the community.")
+Post.create(name: "Eliza", title: "Resources on Cooking", content: "Here are some of my best cooking tips.")
+Post.create(name: "Mike", title: "Seeking Help", content: "Hey y'all, really need some help on a project.")
+Post.create(name: "Sarah", title: "Fun Stuff", content: "I love having fun, some of may favorite things to do are skiing, cooking, and cycling.")
+Post.create(name: "Jordan", title: "Best Sport Ever", content: "I totally love basketball because its so much fun.")
